670027d8e3d35876e3bf69dcb9c2b38d6f9dc8ee
Elie Gambache <elyahou.hadass@gmail.com>
2026-05-10 14:21:30 +0300

    Fix unstaged deleted files

--- plugin-build/plugin/src/main/kotlin/io/github/kdroidfilter/nucleus/ExperimentalNucleusLibrary.kt
+++
@@ -1,8 +0,0 @@
-package dev.nucleusframework.nucleus
-
-// We write explicitly about OptIn, because IDEA doesn't suggest it.
-@RequiresOptIn(
-    "This library is experimental and can be unstable. " +
-        "Add @OptIn(dev.nucleusframework.nucleus.ExperimentalNucleusLibrary::class) annotation.",
-)
-annotation class ExperimentalNucleusLibrary
