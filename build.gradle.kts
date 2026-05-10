84b2b15c750d843ef1e7c999e13acb1c283bc569
Elie Gambache <elyahou.hadass@gmail.com>
2026-05-10 18:09:39 +0300

    chore: wire decorated-window-tao into CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,6 +98,7 @@ tasks.register("preMerge") {
     dependsOn(":decorated-window-core:check")
     dependsOn(":decorated-window-jbr:check")
     dependsOn(":decorated-window-jni:check")
+    dependsOn(":decorated-window-tao:check")
     dependsOn(":decorated-window-jewel:check")
     dependsOn(":decorated-window-material2:check")
     dependsOn(":decorated-window-material3:check")
